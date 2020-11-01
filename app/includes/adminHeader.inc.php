39cd0ab9dde7c3ce303c92e88cff42bbe61e5d5f
RinaBg <73663072+RinaBg@users.noreply.github.com>
2020-11-01 19:39:57 +0200

    Add files via upload

---
+++ app/includes/adminHeader.inc.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="logo">
+        <a href="<?php echo BASE_URL . '/index.php' ?>" class="logo-text"><span>BlogLogo</span></a>
+    </div>
+    <i class="fa fa-bars menu-toggle"></i>
+    <ul class="nav">
+        <li class="nav-item">
+          <a href="#" class="nav-link">
+            <i class="fa fa-user"></i>
+               Marina Boangher
+            <i class="fa fa-chevron-down"></i>
+          </a>
+            <ul>
+              <li class="nav-item">
+                <a href="#" class="logout">Logout</a>
+              </li>
+            </ul>
+        </li>
+    </ul>
+  </header>
---
+++ app/includes/adminSidebar.inc.php
@@ -0,0 +1,7 @@
+<div class="left-sidebar">
+    <ul>
+        <li><a href="<?php echo BASE_URL . '/admin/posts/index.posts.php'; ?>">Manage Posts</a></li>
+        <li><a href="<?php echo BASE_URL . '/admin/topics/index.topics.php'; ?>">Manage Topics</a></li>
+        <li><a href="<?php echo BASE_URL . '/admin/users/index.users.php'; ?>">Manage Users</a></li>
+    </ul>
+  </div>
