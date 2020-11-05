cf8dd6d9bf59d3f4ab6a71e0fb4dfa093cafd49d
RinaBg <73663072+RinaBg@users.noreply.github.com>
2020-11-05 20:43:57 +0200

    Add files via upload

---
+++ app/helpers/validateTopic.php
@@ -0,0 +1,14 @@
+<?php
+
+function validateTopic($topic)
+{
+    $errors = array();
+    if (empty($topic['name'])) {
+        array_push($errors, 'Topic name is required');
+    }
+    $existingTopic = selectOne('topics', ['name' => $topic['name']]);
+    if ($existingTopic) {
+        array_push($errors, 'Topic already exists');
+    }     
+    return $errors;
+}
