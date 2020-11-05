62876bd8e0c9096f8a8820fae6496dda7301ec9f
RinaBg <73663072+RinaBg@users.noreply.github.com>
2020-11-05 20:42:44 +0200

    Update edit.topics.php

--- admin/topics/edit.topics.php
+++ admin/topics/edit.topics.php
@@ -1,4 +1,5 @@
 <?php include("../../path.php") ?>
+<?php include(ROOT_PATH . "/app/controllers/topics.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -44,14 +45,17 @@
 
         <h2 class="page-title">Edit Topic</h2>
 
+        <?php include(ROOT_PATH . "/app/helpers/formErrors.php")?>
+
         <form action="edit.topics.php" method="POST">
+            <input type="hidden" name="id" value="<?php echo $id; ?>" >
             <div>
                 <label>Name</label>
-                <input type="text" name="title" class="text-input">
+                <input type="text" name="name" value="<?php echo $name; ?>" class="text-input">
             </div>
             <div>
                 <label>Description</label>
-                <textarea name="body" id="body"></textarea>
+                <textarea name="description" id="body"><?php echo $description; ?></textarea>
                 <script>
                   ClassicEditor.create(document.querySelector("#body")).catch(
                     (error) => {
@@ -61,7 +65,7 @@
                 </script>
             </div>
             <div>
-                <button type="submit" class="btn btn-big">Update Topic</button>
+                <button type="submit" name="update-topic" class="btn btn-big">Update Topic</button>
             </div>
         </form>
         
